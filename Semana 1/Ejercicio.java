d37936db9a6b7e550c85a5b99ef2b2b9997512db
mariapauli22 <maria.varelao@campusucc.edu.co>
2025-02-17 14:37:06 +0000

    Ejercicio1

---
+++ Semana 1/Ejercicio.java
@@ -0,0 +1,32 @@
+public class Ejercicio {
+
+    public static void main(String[] args) {
+
+
+        //solicitar la variable n por teclado
+        scanner leer = new scanner (System.in);
+        System.out.println("Por favor digite la cantidad de elementos del arreglos: ");
+        int n =leer.nextInt ();
+        //int n=5;
+        int [] b=new int [n];
+         
+
+
+
+
+
+        //sumar los elementos del arreglo
+        int suma = 0;
+        for (int i = 0;i < b.length; i++){
+            suma = suma + b[i];
+
+
+        
+
+
+
+
+      }
+  }
+
+}
