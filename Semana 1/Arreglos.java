589b12407db7abc6e1db859ce3e61c74b5e382b7
mariapauli22 <maria.varelao@campusucc.edu.co>
2025-02-17 13:44:46 +0000

    Edicion

--- Semana 1/Arreglos.java
+++ Semana 1/Arreglos.java
@@ -1,9 +1,22 @@
 public class Arreglos {
     public static void main (String[] args){
 
+//------------------------------------
 
-        
+int tam = 10;
+int[]b = new int[tam]
 
+//Estructura para llenar el arreglo,con numeros aleatorios
+//entre 10 y 30
+
+
+for(int i = 0;i < b.length; i++){
+    b[i] = (int) (math.random() * (30 + 0 )) - 0;
+    
+
+    for (int i=0;i < b.length; i++){
+        System.out.println("b["+i+"]="+b[i]);
 
     }
+    
 }
