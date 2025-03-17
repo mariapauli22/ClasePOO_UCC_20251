b045dec132dc7e0731574947cc695875942b3c24
mariapauli22 <maria.varelao@campusucc.edu.co>
2025-03-17 14:03:13 +0000

    Creacion objetos clase

--- Clase 7/Ejecutar.java
+++ Clase 7/Ejecutar.java
@@ -1,5 +1,9 @@
 public class Ejecutar {
     public static void main(String[] args) {
-        
+    
+    //Instanciar el objeto (creación)
+    Estudiante objEst1 = Estudiante (1, "Gabriela", "Astorquiza", 15, "Ingeniera");
+    Estudiante objEst2 = Estudiante (2,"Juan", "Castro",20, "Derecho");
+
     }
 }
