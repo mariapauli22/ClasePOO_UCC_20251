57debfb2f23d4be1b9308982f694f2f0fc6c3d0e
mariapauli22 <maria.varelao@campusucc.edu.co>
2025-05-19 09:28:53 +0000

    personaj

---
+++ Trabajo f/Personaje.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public abstract class Personaje {
+    protected String nombre;
+    protected int puntosDeVida;
+    protected final int MAX_DANO = 30;
+    protected final int MIN_DANO = 10;
+
+    public Personaje(String nombre) {
+        this.nombre = nombre;
+        this.puntosDeVida = 100;
+    }
+
+    public void atacar(Personaje oponente) {
+        Random rand = new Random();
+        int dano = rand.nextInt((MAX_DANO - MIN_DANO) + 1) + MIN_DANO;
+        oponente.recibirDano(dano);
+        System.out.println(nombre + " ataca a " + oponente.getNombre() + " causando " + dano + " puntos de daño.");
+    }
+
+    public void recibirDano(int dano) {
+        puntosDeVida -= dano;
+        if (puntosDeVida < 0) puntosDeVida = 0;
+    }
+
+    public boolean estaVivo() {
+        return puntosDeVida > 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntosDeVida() {
+        return puntosDeVida;
+    }
+}
